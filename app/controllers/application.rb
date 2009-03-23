53aea8294980a69c0eada2c7e3f8cfaf11157807
Matt Knopp <matt.knopp@gmail.com>
2009-03-23 00:31:30 -0700

    remove defaults that won't matter for this project

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,19 +3,23 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '601406ea5aec96a2c045c3db36853eda'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
-  
+  before_filter :dispose_user
   before_filter :authenticate
   
+  # convince method available to views so you don't have to say User.current_user
+  def current_user
+    User.current_user
+  end
+  
+  
   protected
+  
+  # make sure current_user isn't the user from the last request
+  def dispose_user
+    User.current_user = nil
+  end
+  
   def authenticate
     authenticate_or_request_with_http_basic("YAMS-REST-API") do |username,password|
       User.authenticate(username, password)
