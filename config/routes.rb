c1e8fecd91a1cfc8f084d55e49056836e3187721
Matt Knopp <matt.knopp@gmail.com>
2009-03-24 03:14:10 -0700

    sigh, s/delete/destroy/

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.resources :users,       :only => [:index, :show                  ]
-  map.resources :invites,     :only => [:index, :show, :create, :delete]
-  map.resources :memberships, :only => [:index,        :create, :delete]
+  map.resources :users,       :only => [:index, :show                   ]
+  map.resources :invites,     :only => [:index, :show, :create, :destroy]
+  map.resources :memberships, :only => [:index,        :create, :destroy]
   
-  map.resources :events,      :only => [:index, :show, :create, :delete]
-  map.resources :groups,      :only => [:index, :show, :create, :delete]
-  map.resources :projects,    :only => [:index, :show, :create, :delete]
+  map.resources :events,      :only => [:index, :show, :create, :destroy]
+  map.resources :groups,      :only => [:index, :show, :create, :destroy]
+  map.resources :projects,    :only => [:index, :show, :create, :destroy]
   
 end
