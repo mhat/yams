4e775240659940cf6bf812876f7a8140919f8777
Matt Knopp <matt.knopp@gmail.com>
2009-03-24 01:23:40 -0700

    discovered rescue_action, just what I was was looking for, sweet

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,14 +4,27 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   
+  ## filters
   before_filter :dispose_user
   before_filter :authenticate
   
-  # convince method available to views so you don't have to say User.current_user
+  # convince method available to all controllers
   def current_user
     User.current_user
   end
   
+  
+  def rescue_action (ex)
+    respond_to do |format| 
+      case ex
+      when ActiveRecord::RecordNotFound then
+        format.json { render :json => [{'status' => 404}]  }
+      else
+        ## handles any raised exception with a 500
+        format.json { render :json => [{'status' => 500}]  }
+      end
+    end
+  end
   
   protected
   
