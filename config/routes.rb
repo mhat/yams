d1c8b8a4d013df7eb73389f7fc54b8a7468ef5e5
Matt Knopp <matt.knopp@gmail.com>
2009-03-24 03:45:51 -0700

    added update to invites route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.resources :users,       :only => [:index, :show                   ]
-  map.resources :invites,     :only => [:index, :show, :create, :destroy]
+  map.resources :invites,     :only => [:index, :show, :create, :update, :destroy]
   map.resources :memberships, :only => [:index,        :create, :destroy]
   
   map.resources :events,      :only => [:index, :show, :create, :destroy]
